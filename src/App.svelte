fd5033e075386ddbef2b0b69219aee78548ff4de
BASTIEN PIEDALLU <bastien.piedallu@gmail.com>
2023-11-27 00:15:03 +0100

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Button from './lib/Button.svelte';
+  import Input from './lib/Input.svelte';
+</script>
+
+<main>
+  <Button />
+  <Input />
+  <button>Mon bouton 2</button>
+</main>
+
+<style>
+</style>
---
+++ src/lib/Input.svelte
@@ -0,0 +1,5 @@
+<script>
+  let value = '';
+</script>
+
+<input type="text" bind:value on:input={() => console.log(value)} />
